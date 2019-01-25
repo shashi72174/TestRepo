ebf43142f3b4bd9515f305e70fc144afb34200f5
Shashi <shashi72174@gmail.com>
2019-01-26 00:34:16 +0530

    done with sts workspace

--- Test/src/com/testing/Test.java
+++ Test/src/com/testing/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloWorld!!!");
-		
+		System.out.println("changes from sts workspace");
 	}
 
 }
